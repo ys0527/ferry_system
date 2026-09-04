f95bba2b28141edf4855e98a0c8cce264de68229
Ron <ronleehonglim2@gmail.com>
2026-09-04 22:13:10 +0800

    Merge

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,10 +5,13 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
   file_selector_windows
+  screen_brightness_windows
+  share_plus
   url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
