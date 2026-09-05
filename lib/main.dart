a47a52ae4bc84d5afb5bc61ce92f0b057298d1fe
ys0527 <tanyongsheng2004@gmail.com>
2026-09-05 19:19:06 +0800

    change supabase to public key

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Stripe.publishableKey =
   'pk_test_51UATPFFWH2R2KmW9YBGLxfmMpnJWyvcV3zVMget5tHVcctlAnhA1MGmEnydjIk2hTDiCsHi3o8dtQaO4bFuHJBUe00baDR3Ke8';
-  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
   runApp(const MyApp());
 }
 
--- lib/supabase_config.dart
+++ lib/supabase_config.dart
@@ -1,6 +1,6 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 const String supabaseUrl = 'https://nprcxfvipxnjaecufhcz.supabase.co';
-const String supabaseKey = 'sb_secret_QOYG1HmTZDSkm6ANOD1_fw_x2l2J6LG';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wcmN4ZnZpcHhuamFlY3VmaGN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyNTkzOTksImV4cCI6MjEwMTgzNTM5OX0.YBpbQ736kdFlIfkzttloW7xHOY3Bn1efC8QVp35TL2I';
 
 SupabaseClient get supabase => Supabase.instance.client;
